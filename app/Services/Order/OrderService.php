3ed166f20c33884e33a7988dc318650f52b69154
amiragyan <artyom.amiragyan@yandex.ru>
2025-08-22 01:19:44 +0300

    fix: first last name

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -79,8 +79,8 @@ class OrderService
             auth()->user()->update(
                 [
                     'phone' => $orderArray['phone'],
-                    'first_name' => $orderArray['first_name'],
-                    'last_name' => $orderArray['last_name'],
+                    'last_name' => $orderArray['first_name'],
+                    'first_name' => $orderArray['last_name'],
                     'email' => $orderArray['email'],
                 ]
             );
