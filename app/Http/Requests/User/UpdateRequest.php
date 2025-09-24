91e404070f6b782c8bfac5d6451f58671e878d85
masha <mariyapanaeva512@gmail.com>
2025-09-24 18:18:46 +0300

    ix validation

--- app/Http/Requests/User/UpdateRequest.php
+++ app/Http/Requests/User/UpdateRequest.php
@@ -7,8 +7,7 @@ use Illuminate\Validation\Rule;
 
 class UpdateRequest extends FormRequest
 {
-    const MIN_AGE = 14;
-    const MAX_AGE = 120;
+    const MAX_AGE = 105;
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -26,10 +25,10 @@ class UpdateRequest extends FormRequest
     {      
         return [
             'phone' => ['string',  Rule::unique('users')->ignore($this->user()->id), 'regex:/^\+375(25|29|33|44)\-\d{3}\-\d{2}\-\d{2}$/'],
-            'first_name' => ['required', 'string','min:2','regex:/^[a-zA-Zа-яА-ЯёЁ\s\-]+$/u'],
-            'last_name' => ['required', 'string', 'min:2','regex:/^[a-zA-Zа-яА-ЯёЁ\s\-]+$/u'],
+            'first_name' => ['nullable', 'string','min:2','regex:/^[a-zA-Zа-яА-ЯёЁ\s\-]+$/u'],
+            'last_name' => ['nullable', 'string', 'min:2','regex:/^[a-zA-Zа-яА-ЯёЁ\s\-]+$/u'],
             'gender' => ['nullable', 'string'],
-            'birthday' => ['nullable', 'date', 'before_or_equal:' . now()->subYears(self::MIN_AGE)->format('Y-m-d'), 'after_or_equal:' . now()->subYears(self::MAX_AGE)->format('Y-m-d')],
+            'birthday' => ['nullable', 'date', 'before_or_equal:' . now()->format('Y-m-d'), 'after_or_equal:' . now()->subYears(self::MAX_AGE)->format('Y-m-d')],
             'email' => ['nullable', 'email'],
             'old_password' => ['nullable', 'string'],
             'new_password' => ['nullable', 'string'],
