00f8e1df784299dfbf69c6d37d05f78e6dfed365
masha <mariyapanaeva512@gmail.com>
2025-09-29 13:44:12 +0300

    фикс создания пустого заказа

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -35,7 +35,7 @@ class OrderController extends Controller
             'intercom' => 'nullable|string|max:20',
             'comment' => 'nullable|string|max:1000',
             'ids' => 'required|array|min:1',
-            'ids.*' => 'integer',
+            'ids.*' => 'integer|exists:evo_site_content,id',
             'promocodes' => 'nullable|array',
             'promocodes.*' => 'string|max:50',
         ]);
