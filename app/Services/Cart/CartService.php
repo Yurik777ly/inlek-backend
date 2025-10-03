3ce8614cd98696b3a0289b36ecdc6a448f59a7a9
masha <mariyapanaeva512@gmail.com>
2025-10-03 15:42:19 +0300

    Revert "пробное решение с параллельным запросом"
    This reverts commit f7390ad17e735aff03c6177ee5ae1c3508517f13.

--- app/Services/Cart/CartService.php
+++ app/Services/Cart/CartService.php
@@ -17,7 +17,6 @@ use App\Models\CartProductPharmacyView;
 use App\Models\CartsDetailed;
 use Mockery\Matcher\Any;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
-use Illuminate\Support\Facades\Cache;
 
 class CartService
 {
@@ -154,26 +153,11 @@ class CartService
 
     public function getCartDetailed(CartDetailedDTO $cartDTO)
     {
-
-        $lockKey = 'cart_detailed:' . auth()->user()->id;
-        if (Cache::has($lockKey)) {
-            return response()->json(['error' => 'Request in progress'], 429);
-        }
-
-        Cache::put($lockKey, true, 1);
-
         $cart = $this->Cart->query()->where('user_id', auth()->user()->id)->first();
-        if (
-            $cart->pharmacy_id != $cartDTO->pharmacyId || 
-            $cart->promocodes != $cartDTO->promocodes ||
-            $cart->delivery_zone != $cartDTO->deliveryZone ?? null
-        ) {
-            $cart->pharmacy_id = $cartDTO->pharmacyId;
-            $cart->promocodes = $cartDTO->promocodes;
-            $cart->delivery_zone = $cartDTO->deliveryZone ?? null;
-            $cart->save();
-        }
-
+        $cart->pharmacy_id = $cartDTO->pharmacyId;
+        $cart->promocodes = $cartDTO->promocodes;
+        $cart->delivery_zone = $cartDTO->deliveryZone ?? null;
+        $cart->save();
 
         $dataModel = $this->CartsDetailed
             ->query()
@@ -414,22 +398,10 @@ class CartService
 
     public function getCartDetailedWithoutChoosenPharm(CartDetailedDTO $cartDTO)
     {
-        $lockKey = 'cart_detailed:' . auth()->user()->id;
-        if (Cache::has($lockKey)) {
-        return response()->json(['error' => 'Request in progress'], 429);
-        }
-
-        Cache::put($lockKey, true, 1);
-
         $cart = $this->Cart->query()->where('user_id', auth()->user()->id)->first();
-        if ( 
-            $cart->promocodes != $cartDTO->promocodes ||
-            $cart->delivery_zone != $cartDTO->deliveryZone ?? null
-        ) {
-            $cart->promocodes = $cartDTO->promocodes;
-            $cart->delivery_zone = $cartDTO->deliveryZone ?? null;
-            $cart->save();
-        }
+        $cart->promocodes = $cartDTO->promocodes;
+        $cart->delivery_zone = $cartDTO->deliveryZone ?? null;
+        $cart->save();
 
         $data =$this->getCart();
 
