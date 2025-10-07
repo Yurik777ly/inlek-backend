eaacf7d500404e01629a828962aa634d152559b7
masha <mariyapanaeva512@gmail.com>
2025-10-07 16:31:21 +0300

    добавлена проверка на пустоту

--- app/Services/Cart/CartService.php
+++ app/Services/Cart/CartService.php
@@ -443,31 +443,34 @@ class CartService
             'availability_priority' => 0
         ];
         $totalSum = 0;
-        foreach ($products as $product ) {
-            $sumProduct = round($product['quantity']*$product['product_charachters']['product_price_from'],2);
-            $totalSum = $totalSum+$sumProduct;
-            $finalArray['cart']['products'][] = [
-                'prices' => [
-                    'price' => round($product['product_charachters']['product_price_from'], 2),
-                    'final_price' => round($product['product_charachters']['product_price_from'], 2),
-                    'price_old' => round($product['product_charachters']['product_price_from'], 2),
-                ],
-                'quantity' => $product['quantity'],
-                'product_id' => $product['product_charachters']['product_id'],
-                'availability' => 'full',
-                'action_json' => $product['action_json'],
-                'product_info' => $product['product_charachters'],
-                'product_totals' =>  [
-                    "total" => $sumProduct,
-                    "total_old" => $sumProduct,
-                    "final_total" => $sumProduct,
-                    "final_total_with_promos" => $sumProduct,
-                    "total_discount_only_promos" => 0.0,
-                    "total_discount_with_promos" => 0.0,
-                    "total_discount_without_promos" => 0.0,
-                ],
-            ];
+        if (is_array($products)) {
+            foreach ($products as $product ) {
+                $sumProduct = round($product['quantity']*$product['product_charachters']['product_price_from'],2);
+                $totalSum = $totalSum+$sumProduct;
+                $finalArray['cart']['products'][] = [
+                    'prices' => [
+                        'price' => round($product['product_charachters']['product_price_from'], 2),
+                        'final_price' => round($product['product_charachters']['product_price_from'], 2),
+                        'price_old' => round($product['product_charachters']['product_price_from'], 2),
+                    ],
+                    'quantity' => $product['quantity'],
+                    'product_id' => $product['product_charachters']['product_id'],
+                    'availability' => 'full',
+                    'action_json' => $product['action_json'],
+                    'product_info' => $product['product_charachters'],
+                    'product_totals' =>  [
+                        "total" => $sumProduct,
+                        "total_old" => $sumProduct,
+                        "final_total" => $sumProduct,
+                        "final_total_with_promos" => $sumProduct,
+                        "total_discount_only_promos" => 0.0,
+                        "total_discount_with_promos" => 0.0,
+                        "total_discount_without_promos" => 0.0,
+                    ],
+                ];
+            }
         }
+
         $finalArray['cart']['totals']['total_price_old'] = round($totalSum, 2);
         $finalArray['cart']['totals']['total_final_price'] = round($totalSum, 2);
 
